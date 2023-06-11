efe29a72cfe3bba09d7370b36aa30fb0c54f9929
Neyaz Wakil <neyazwakil8271@gmail.com>
2023-06-11 20:04:44 +0530

    this project is to send mail using java cod

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sending mail done please check your mail");
+MailHandler mailHandler=new MailHandler();
+mailHandler.sendmail();
+
+    }
+}
