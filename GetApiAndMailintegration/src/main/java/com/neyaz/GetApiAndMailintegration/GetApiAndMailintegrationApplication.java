268ac02dfe08b7d14456b2ec5387bf41caa51c1f
Neyaz Wakil <neyazwakil8271@gmail.com>
2023-06-23 23:59:22 +0530

    mailIntegration

---
+++ GetApiAndMailintegration/src/main/java/com/neyaz/GetApiAndMailintegration/GetApiAndMailintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.neyaz.GetApiAndMailintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetApiAndMailintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetApiAndMailintegrationApplication.class, args);
+	}
+
+}
